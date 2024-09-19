ed2122c83814cac9ea6b034dd908f0569c640e0b
mighty808 <effahaboagye888@gmail.com>
2024-09-19 23:49:32 +0000

    some projects and tuts

---
+++ index.js
@@ -0,0 +1,9 @@
+// let fahrenhiet;
+//         function convertToCelsius (fahrenhiet){
+//             celsius = (fahrenhiet - 32) * 5/9;
+//             return celsius
+//         }
+
+//         convertToCelsius(80);
+
+
